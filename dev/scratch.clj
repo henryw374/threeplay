a2f6c41881fb058d0fe1ca21398fbe67bec12600
Henry Widd <henryw374@gmail.com>
2024-06-26 12:35:37 +0100

    tidy

--- dev/scratch.clj
+++ dev/scratch.clj
@@ -12,23 +12,46 @@
 
 
 
-
+; 1 + 1 = .... 
 (+ 1 1)
 
 
 
 
-(str "one plus one is " (+ 1 1))
+(- 5 4)
+
+
+
+(str "one plus one is " (+ 1 1 (* 6 5)))
+
+(str (+ 1 1))
 
 
 (greet "Park Mead")
 
 
 
-
-
 (defn greet [who]
   (str "hello " who " and everybody else"))
+
+
+
+
+
+
+
+
+
+
+
+
+{:date            "2024-12-17"
+ :hourly-forecast [
+                   {:time "06:00" :temperature -5 :precipitation "light-snow"}
+                   {:time "07:00" :temperature -4 :precipitation "heavy-snow"}
+                   {:time "08:00" :temperature -3 :precipitation "none"}
+                   ]}
+
 
 
 
--- src/threeplay/play/play1.cljs
+++ src/threeplay/play/play1.cljs
@@ -17,7 +17,7 @@
                     (* 2 js/Math.PI)))
                 (set! (.. mesh-ref -current -position -y) (js/Math.sin pos))
                 (set! (.. mesh-ref -current -position -x) (js/Math.cos pos))
-                (set! (.. mesh-ref -current -rotation -z) (* pos (/ 180 js/Math.PI 100)))))
+                (set! (.. mesh-ref -current -rotation -z) (* pos (/ 180 js/Math.PI 75)))))
     ($ :mesh
       (assoc props
         :ref mesh-ref
@@ -32,9 +32,9 @@
         {:color (if hovered "hotpink" "orange")}))))
 
 (defui view [_props]
-  ($ :div {:style {:border "solid"
+  ($ :div {:style { ;:border "solid"
                    :height "400px"}}
-    ($ Canvas
+    ($ Canvas 
       ($ :ambientLight {:intensity 1})
       ($ :spotLight {:position  #js[50 10 10]
                      :angle     0.15
@@ -43,8 +43,10 @@
                      :intensity js/Math.PI})
       ($ :pointLight {:position  #js[-10 -10 -10]
                       :decay     0
-                      :intensity js/Math.PI})
+                      :intensity 2})
       ($ Box {:position #js[1 0 2]
               :rotation #js[0 1 0]})
+      ; ($ Box {:position #js[2.2 0 2]})
+      ;($ Box {:position #js[2.2 0 3]})
       ;($ Box {:position #js[2.2 0 2]})
       )))
